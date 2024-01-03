6a076cd595f1684e7ccf2aa8ba6524cacf676bbf
Alibek <alibek.keneskhanov@gmail.com>
2024-01-03 15:09:15 +0600

    fixed db creation

--- create_db.sql
+++ create_db.sql
@@ -1,3 +1,20 @@
 CREATE TABLE users (
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       name VARCHAR(50),
+                       email VARCHAR(50),
+                       password VARCHAR(50)
+);
 
-)
+CREATE TABLE users_phone_book (
+                                  id INT PRIMARY KEY AUTO_INCREMENT,
+                                  name VARCHAR(50),
+                                  description TEXT,
+                                  user_id INT REFERENCES users(id)
+);
+
+CREATE TABLE users_phones (
+                              id INT PRIMARY KEY AUTO_INCREMENT,
+                              name VARCHAR(50),
+                              phone VARCHAR(12),
+                              phone_book_id INT REFERENCES users_phone_book(id)
+);
